ef0acaa6d8c2e14b0686d295ac10b6e59db3d01f
T0312 <94551519+T0312@users.noreply.github.com>
2023-10-16 19:35:44 -0400

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,281 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ExpenseTrackerApp());
+}
+
+class ExpenseTrackerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      darkTheme: ThemeData.dark(), // Use dark theme here
+      home: ExpenseDashboard(),
+    );
+  }
+}
+
+class ExpenseDashboard extends StatefulWidget {
+  @override
+  _ExpenseDashboardState createState() => _ExpenseDashboardState();
+}
+
+class _ExpenseDashboardState extends State<ExpenseDashboard> {
+  // Sample weekly income
+  double weeklyIncome = 1000.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker Dashboard'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.35, // Set a fixed width (35% of screen width)
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Total Expenses',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      // Wrap the expense amount in a decorated container
+                      Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(
+                          '\$500.00',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.35, // Set a fixed width (35% of screen width)
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Weekly Income',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      // Wrap the weekly income in a decorated container
+                      Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.green, 
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(
+                          '\$$weeklyIncome',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Expense Categories',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                CategoryCard(
+                  categoryName: 'Food',
+                  amountSpent: 150.00,
+                  color: Colors.green,
+                ),
+                CategoryCard(
+                  categoryName: 'Transportation',
+                  amountSpent: 100.00,
+                  color: Colors.orange,
+                ),
+                CategoryCard(
+                  categoryName: 'Shopping',
+                  amountSpent: 200.00,
+                  color: Colors.purple,
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Recent Transactions',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(height: 10),
+            TransactionCard(
+              transactionName: 'Restaurant',
+              amount: 50.00,
+              date: 'Oct 1, 2023',
+              icon: Icons.fastfood,
+              categoryColor: Colors.green,
+            ),
+            TransactionCard(
+              transactionName: 'Gas Station',
+              amount: 30.00,
+              date: 'Sep 30, 2023',
+              icon: Icons.local_gas_station,
+              categoryColor: Colors.orange,
+            ),
+            TransactionCard(
+              transactionName: 'Electronics Store',
+              amount: 120.00,
+              date: 'Sep 29, 2023',
+              icon: Icons.shopping_cart,
+              categoryColor: Colors.purple,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add expense functionality here
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String categoryName;
+  final double amountSpent;
+  final Color color;
+
+  CategoryCard({
+    required this.categoryName,
+    required this.amountSpent,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      width: MediaQuery.of(context).size.width * 0.28, 
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            categoryName,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            '\$$amountSpent',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TransactionCard extends StatelessWidget {
+  final String transactionName;
+  final double amount;
+  final String date;
+  final IconData icon;
+  final Color categoryColor;
+
+  TransactionCard({
+    required this.transactionName,
+    required this.amount,
+    required this.date,
+    required this.icon,
+    required this.categoryColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ListTile(
+        leading: Icon(
+          icon,
+          size: 45,
+          color: categoryColor,
+        ),
+        title: Text(
+          transactionName,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(
+          date,
+          style: TextStyle(
+            color: Colors.grey,
+          ),
+        ),
+        trailing: Text(
+          '\$$amount',
+          style: TextStyle(
+            fontSize: 25, 
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+}
